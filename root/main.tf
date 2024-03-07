db07990751c6e50fe3c15959cb95bc6ae045ccdb
ismailostmar <ismailostmar@gmail.com>
2024-03-07 12:39:44 +0100

    test

--- root/main.tf
+++ root/main.tf
@@ -18,7 +18,8 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.network.name
   resource_group_name  = azurerm_resource_group.myRG.name
   address_prefixes     = ["10.0.1.0/24"]
-}
+} 
+
 
 resource "tls_private_key" "key" {
   algorithm = "RSA"
